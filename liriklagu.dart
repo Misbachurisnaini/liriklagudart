42e08d1790e22e9391b795332a55f64532e4683c
Misbachurisnaini <aninisnaini214@gmail.com>
2022-03-10 20:21:53 +0700

    tugas minggu 3 membuat lirik lagu

---
+++ liriklagu.dart
@@ -0,0 +1,9 @@
+import 'dart:async' ;
+void main(List<String> args) {
+  print("Bertahan Terluka Oleh Fabio Asher");
+  print("-----------------------------------");
+  print("Andaikan kau tahu, rasa sayangku");
+  Timer(Duration(seconds: 7), () => print("Mengapa aku tetap disini"));
+  Timer(Duration(seconds: 3), () => print("Melebihi rasa sakit ini"));
+  Timer(Duration(seconds: 5), () => print("Mungkin kau takkan pernah menyangka"));
+}
